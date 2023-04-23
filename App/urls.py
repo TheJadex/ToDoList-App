d98fb8fd44994bd3214a47d48b191bea4190bee4
jade <moyosoreoluwamafo@gmail.com>
2023-04-23 17:10:06 +0100

    edited folder

---
+++ App/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# Naming application to make linking between different views/apps easier and to eliminate namespace collisions.
+app_name = "tasks"
+
+urlpatterns = [
+    # name = "name" --> here, i am giving the views funtion a name
+    path("", views.name, name="name"),
+]
